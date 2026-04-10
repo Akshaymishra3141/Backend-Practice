c4706529b9aef2e9c56bf061642184979ce7d4c1
Akshay Kumar Mishra <157806392+Akshaymishra3141@users.noreply.github.com>
2026-04-10 09:26:54 +0530

    feat: install and configure dotenv for environment variable management

--- 01Deploy/index.js
+++ 01Deploy/index.js
@@ -1,1 +1,20 @@
-console.log("Akshay Learning Backend")
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send('Hello Akshay')
+})
+
+app.get('/twitter', (req,res)=>{
+    res.send('<h1>Twitter</h1>')
+})
+app.get('/login', (req,res)=>{
+    res.send('<h1>Please Login to this Web Site</h1>')
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
+})
