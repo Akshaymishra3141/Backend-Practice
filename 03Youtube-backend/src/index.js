5e7f6bc24d234267e29fb9489b43c9a8c13c6170
Akshay Kumar Mishra <157806392+Akshaymishra3141@users.noreply.github.com>
2026-04-12 18:54:14 +0530

    feat: add cookie-parser and cors middleware, implement async error handling, and create ApiError and ApiResponse utility classes

--- 03Youtube-backend/src/index.js
+++ 03Youtube-backend/src/index.js
@@ -5,23 +5,13 @@ import connectDB from "./db/index.js";
 
 const app = express();
 
-connectDB();
-/*
-;(async()=>{
-    try{
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log("Connected to DB successfully");
-        app.on("error", (err)=>{
-            console.log("Error ", err);
-            throw err
-        })
-        app.listen(process.env.PORT, ()=>{
-            console.log(`Server is running on port ${process.env.PORT}`);
-        })
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||3000,()=>{
+        console.log("Server is running on port ", process.env.PORT);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection Failed !!!", err)
+})
 
-    }catch(err){
-        console.log("Error in connecting to DB", err);
-        throw
-    }
-})()
-*/
